4daad4eac353f11b416121b2dbe49dfef265024d
James Ho <jamesho22@Jamess-MacBook-Pro.local>
2020-11-30 14:43:15 -0800

    compute optimal control trajectory given current state

---
+++ extmodel.m
@@ -0,0 +1,21 @@
+% Create a simple continous model
+Ac = [0 1 0; 3 0 1; 0 1 0];
+Bc = [1; 1; 3];
+Cc = [0 1 0];
+Dc = zeros(1,1);
+
+% Covert the continuous model to a discrete model by sampling every 1
+% second
+Delta_t = 1;
+[Ad, Bd, Cd, Dd] = c2dm(Ac, Bc, Cc, Dc, Delta_t);
+
+% Create the augmented state space model
+[m1, n1] = size(Cd);
+[n1, n_in] = size(Bd);
+A_e = eye(n1+m1, n1+m1);
+A_e(1:n1, 1:n1) = Ad;
+B_e = zeros(n1+m1, n_in);
+Be(1:n1, :) = Bd;
+Be(n1+1:n1+m1, :) = Cd*Bd;
+C_e = zeros(m1, n1+m1);
+C_e(:, n1+1:n1+m1) = eye(m1, m1);
---
+++ mpcgain.m
@@ -0,0 +1,50 @@
+function [Phi_Phi, Phi_F, Phi_R, A_e, B_e, C_e] = mpcgain(Ap, Bp, Cp, Nc, Np)
+% input to the function is the state space model.
+% Ap, Bp, Cp: state space model
+% Nc, control horizon
+% Np, prediction horizon
+
+% Create the augmented state space model
+[m1, n1] = size(Cp);
+[n1, n_in] = size(Bp);
+A_e = eye(n1+m1, n1+m1);
+A_e(1:n1, 1:n1) = Ap;
+A_e(n1+1:n1+m1, 1:n1) = Cp*Ap;
+B_e = zeros(n1+m1, n_in);
+B_e(1:n1, :) = Bp;
+B_e(n1+1:n1+m1,:) = Cp*Bp;
+C_e = zeros(m1, n1+m1);
+C_e(:, n1+1:n1+m1) = eye(m1, m1);
+
+
+% compute the F and Phi matricies
+n=n1+m1;
+h(1,:) = C_e;
+F(1,:) = C_e*A_e;
+for kk=2:Np
+    h(kk, :) = h(kk-1, :)*A_e;
+    F(kk, :) = F(kk-1, :)*A_e;
+end
+
+v = h*B_e;
+Phi = zeros(Np, Nc);
+Phi(:, 1) = v;
+for i=2:Nc
+    Phi(:, i) = [zeros(i-1, 1); v(1:Np-i+1, 1)]; % Toeplotz matrix
+end
+
+
+
+BarRs = ones(Np, 1);
+Phi_Phi = Phi'*Phi;
+Phi_F = Phi'*F;
+Phi_R = Phi'*BarRs;
+
+% Calculate the optimal control signal given rw
+rw = 0;
+inv(Phi_Phi + rw*eye(4,4))*(Phi_R - Phi_F*[0.1 0.2]')
+
+
+
+
+
